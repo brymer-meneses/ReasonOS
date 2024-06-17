cfba9d0d68649b699c36eddbc0ec8e3b13810926
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2024-06-17 21:40:10 +0800

    refactor `build.zig`

--- build.zig
+++ build.zig
@@ -6,30 +6,84 @@ const SupportedArchs = enum {
 
 pub fn build(b: *std.Build) void {
     const arch = b.option(SupportedArchs, "arch", "Target Architecture") orelse .x86_64;
-
     const optimize = b.standardOptimizeOption(.{});
+
+    const kernel = configure_kernel(b, arch, optimize);
+    const iso = prepare_iso(b, kernel, arch, optimize);
+
+    {
+        const compile_kernel = b.step("kernel", "Compile the kernel");
+        compile_kernel.dependOn(&kernel.step);
+    }
+
+    {
+        const create_iso = b.step("iso", "Create the iso");
+        create_iso.dependOn(&iso.step);
+    }
+
+    {
+        const run_iso = b.step("run", "Run the ISO in QEMU");
+        const qemu = b.addSystemCommand(&.{
+            "qemu-system-" ++ @tagName(arch),
+            "-serial",
+            "stdio",
+            "-cdrom",
+        });
+        qemu.addFileArg(iso.source);
+        run_iso.dependOn(&qemu.step);
+    }
+}
+
+pub fn configure_kernel(b: *std.Build, arch: SupportedArchs, optimize: std.builtin.OptimizeMode) *std.Build.Step.Compile {
     const limine_zig = b.dependency("limine_zig", .{});
-
     const target = configure_target(b, arch);
-
-    const kernel = b.addExecutable(.{
-        .name = "kernel",
-        .root_source_file = b.path("kernel/main.zig"),
-        .target = target,
-        .optimize = optimize,
-        .code_model = .kernel,
-        .pic = true,
-    });
-
-    kernel.root_module.addImport("limine", limine_zig.module("limine"));
-    kernel.want_lto = false; // Disable LTO. This prevents issues with limine requests
 
     switch (arch) {
         .x86_64 => {
+            const kernel = b.addExecutable(.{
+                .name = "kernel",
+                .root_source_file = b.path("kernel/main.zig"),
+                .target = target,
+                .optimize = optimize,
+                .code_model = .kernel,
+                .pic = true,
+            });
+
+            kernel.root_module.addImport("limine", limine_zig.module("limine"));
+            kernel.want_lto = false; // Disable LTO. This prevents issues with limine requests
             kernel.setLinkerScriptPath(b.path("kernel/arch/x86_64/linker.ld"));
+            return kernel;
         },
     }
+}
 
+pub fn configure_target(b: *std.Build, arch: SupportedArchs) std.Build.ResolvedTarget {
+    var target: std.zig.CrossTarget = .{
+        .cpu_arch = .x86_64,
+        .os_tag = .freestanding,
+        .abi = .none,
+    };
+
+    switch (arch) {
+        .x86_64 => {
+            target.cpu_arch = .x86_64;
+
+            // Disable CPU features that require additional initialization
+            // like MMX, SSE/2 and AVX. That requires us to enable the soft-float feature.
+            const Features = std.Target.x86.Feature;
+            target.cpu_features_sub.addFeature(@intFromEnum(Features.mmx));
+            target.cpu_features_sub.addFeature(@intFromEnum(Features.sse));
+            target.cpu_features_sub.addFeature(@intFromEnum(Features.sse2));
+            target.cpu_features_sub.addFeature(@intFromEnum(Features.avx));
+            target.cpu_features_sub.addFeature(@intFromEnum(Features.avx2));
+            target.cpu_features_add.addFeature(@intFromEnum(Features.soft_float));
+
+            return b.resolveTargetQuery(target);
+        },
+    }
+}
+
+pub fn prepare_iso(b: *std.Build, kernel: *std.Build.Step.Compile, arch: SupportedArchs, optimize: std.builtin.OptimizeMode) *std.Build.Step.InstallFile {
     const limine = b.dependency("limine", .{});
     const limine_exe = b.addExecutable(.{
         .name = "limine",
@@ -79,45 +133,5 @@ pub fn build(b: *std.Build) void {
         iso_name,
     );
 
-    const compile_kernel = b.step("kernel", "Compile the kernel");
-    compile_kernel.dependOn(&kernel.step);
-
-    const create_iso = b.step("iso", "Create the iso");
-    create_iso.dependOn(&iso.step);
-
-    const run_iso = b.step("run", "Run the ISO in QEMU");
-    const qemu = b.addSystemCommand(&.{
-        "qemu-system-" ++ @tagName(arch),
-        "-serial",
-        "stdio",
-        "-cdrom",
-    });
-    qemu.addFileArg(iso.source);
-
-    run_iso.dependOn(&iso.step);
-    run_iso.dependOn(&qemu.step);
-}
-
-pub fn configure_target(b: *std.Build, arch: SupportedArchs) std.Build.ResolvedTarget {
-    switch (arch) {
-        .x86_64 => {
-            var target: std.zig.CrossTarget = .{
-                .cpu_arch = .x86_64,
-                .os_tag = .freestanding,
-                .abi = .none,
-            };
-
-            // Disable CPU features that require additional initialization
-            // like MMX, SSE/2 and AVX. That requires us to enable the soft-float feature.
-            const Features = std.Target.x86.Feature;
-            target.cpu_features_sub.addFeature(@intFromEnum(Features.mmx));
-            target.cpu_features_sub.addFeature(@intFromEnum(Features.sse));
-            target.cpu_features_sub.addFeature(@intFromEnum(Features.sse2));
-            target.cpu_features_sub.addFeature(@intFromEnum(Features.avx));
-            target.cpu_features_sub.addFeature(@intFromEnum(Features.avx2));
-            target.cpu_features_add.addFeature(@intFromEnum(Features.soft_float));
-
-            return b.resolveTargetQuery(target);
-        },
-    }
+    return iso;
 }
